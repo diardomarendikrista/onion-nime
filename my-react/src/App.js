bc7925b69f399245d5d9e99a9bf9936fb4a8aba4
diardomarendikrista <diardomarendikrista@gmail.com>
2021-03-30 11:58:23 +0700

    memindahkan bentuk class ke function masih kurang di bagian search karena waktu tidak cukup, siang akan dilanjutkan lagi.
    tapi sampai saat ini, aman s/d fetchdata & pindah2 page

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,96 +1,39 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AnimeDetail from './components/AnimeDetail.jsx'
 import SearchForm from './components/SearchForm.jsx'
 import { Button, Container } from 'react-bootstrap';
 
+export default function App () {
+  const [animeList, setAnimeList] = useState([]);
+  const [defaultURL] = useState('https://kitsu.io/api/edge/anime?page%5Blimit%5D=12');
+  const [baseURL, setBaseURL] = useState('https://kitsu.io/api/edge/anime?page%5Blimit%5D=12');
+  const [page, setPage] = useState({});
 
-class App extends React.Component {
-  constructor () {
-    super ()
-    
-    this.state = {
-      defaultURL: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=12',
-      baseURL: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=12',
-      animeList: [],
-      page: {}
-    }
-
-  }
-
-  search = async (search) => {
-    if (search) {
-      const keywords = search.split(' ').join('%20')
-      const url = `https://kitsu.io/api/edge/anime?filter[text]=${keywords}`;
-  
-      await this.setState({
-        ...this.state,
-        baseURL: url
-      })
-    } else {
-      await this.setState({
-        ...this.state,
-        baseURL: this.state.defaultURL
-      })
-    }
-    this.fetchData()
-  }
-
-  fetchData = () => {
-    fetch(`${this.state.baseURL}`)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          ...this.state,
-          animeList: res.data,
-          page: res.links
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
-
-  changePage = async (page) => {
-    switch (page) {
+  function changePage (destinationPage) {
+    switch (destinationPage) {
       case 'first':
-        if (!this.state.page.first) console.log(`Page Doesn't Exist`);
+        if (!page.first) console.log(`Page Doesn't Exist`);
         else {
-          await this.setState({
-            ...this.state,
-            baseURL: this.state.page.first
-          })
-          this.fetchData()
+          setBaseURL(page.first)
         }
         break;
       case 'prev':
-        if (!this.state.page.prev) console.log(`Page Doesn't Exist`);
+        if (!page.prev) console.log(`Page Doesn't Exist`);
         else {
-          await this.setState({
-            ...this.state,
-            baseURL: this.state.page.prev
-          })
-          this.fetchData()
+          setBaseURL(page.prev)
         }
         break;
       case 'next':
-        if (!this.state.page.next) console.log(`Page Doesn't Exist`);
+        if (!page.next) console.log(`Page Doesn't Exist`);
         else {
-          await this.setState({
-            ...this.state,
-            baseURL: this.state.page.next
-          })
-          this.fetchData()
+          setBaseURL(page.next)
         }
         break;
       case 'last':
-        if (!this.state.page.last) console.log(`Page Doesn't Exist`);
+        if (!page.last) console.log(`Page Doesn't Exist`);
         else {
-          await this.setState({
-            ...this.state,
-            baseURL: this.state.page.last
-          })
-          this.fetchData()
+          setBaseURL(page.last)
         }
         break;
       default:
@@ -98,48 +41,198 @@ class App extends React.Component {
     }
   }
 
-  componentDidMount () {
-    this.fetchData ()
+  function fetchData () {
+    fetch(`${baseURL}`)
+      .then(res => res.json())
+      .then(res => {
+        setAnimeList(res.data);
+        setPage(res.links);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
- 
-  render () {
-    const { animeList } = this.state;
-    return (
-      <div>
-        <h1 className="page-title text-center">OnioNime</h1>
-        <Container className="text-center">
-          <center>
-            <SearchForm search={this.search}/>
-          </center>
-          {/* tombol page ini nanti sepertinya bisa dipecah jadi component */}
-          <div className="btn-group">
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('first')}>First</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('prev')}>&lt;&lt; Prev</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('next')}>Next &gt;&gt;</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('last')}>Last</Button>
-          </div>
-          <div className="d-flex flex-wrap justify-content-center">
-            {
-              animeList.map(anime => {
-                return (
-                  <AnimeDetail
-                    anime={anime}
-                    key={anime.id}
-                  />
-                )
-              })
-            }
-          </div>
-          <div className="btn-group">
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('first')}>First</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('prev')}>&lt;&lt; Prev</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('next')}>Next &gt;&gt;</Button>
-            <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('last')}>Last</Button>
-          </div>
-          </Container>
-      </div>
-    )
-  }
+
+  useEffect ( () => {
+    fetchData ()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect( () => {
+    fetchData ()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [baseURL])
+
+  return (
+    <div>
+      <h1 className="page-title text-center">OnioNime</h1>
+      <Container className="text-center">
+        <center>
+          {/* <SearchForm search={this.search}/> */}
+        </center>
+        {/* tombol page ini nanti sepertinya bisa dipecah jadi component */}
+        <div className="btn-group">
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('first')}>First</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('prev')}>&lt;&lt; Prev</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('next')}>Next &gt;&gt;</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('last')}>Last</Button>
+        </div>
+        <div className="d-flex flex-wrap justify-content-center">
+          {
+            animeList.map(anime => {
+              return (
+                <AnimeDetail
+                  anime={anime}
+                  key={anime.id}
+                />
+              )
+            })
+          }
+        </div>
+        <div className="btn-group">
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('first')}>First</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('prev')}>&lt;&lt; Prev</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('next')}>Next &gt;&gt;</Button>
+          <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => changePage('last')}>Last</Button>
+        </div>
+        </Container>
+    </div>
+  )
 }
 
-export default App;
+// class App extends React.Component {
+//   constructor () {
+//     super ()
+    
+//     this.state = {
+//       defaultURL: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=12',
+//       baseURL: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=12',
+//       animeList: [],
+//       page: {}
+//     }
+
+//   }
+
+//   search = async (search) => {
+//     if (search) {
+//       const keywords = search.split(' ').join('%20')
+//       const url = `https://kitsu.io/api/edge/anime?filter[text]=${keywords}`;
+  
+//       await this.setState({
+//         ...this.state,
+//         baseURL: url
+//       })
+//     } else {
+//       await this.setState({
+//         ...this.state,
+//         baseURL: this.state.defaultURL
+//       })
+//     }
+//     this.fetchData()
+//   }
+
+//   fetchData = () => {
+//     fetch(`${this.state.baseURL}`)
+//       .then(res => res.json())
+//       .then(res => {
+//         this.setState({
+//           ...this.state,
+//           animeList: res.data,
+//           page: res.links
+//         })
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       })
+//   }
+
+// changePage = async (page) => {
+//   switch (page) {
+//     case 'first':
+//       if (!this.state.page.first) console.log(`Page Doesn't Exist`);
+//       else {
+//         await this.setState({
+//           ...this.state,
+//           baseURL: this.state.page.first
+//         })
+//         this.fetchData()
+//       }
+//       break;
+//     case 'prev':
+//       if (!this.state.page.prev) console.log(`Page Doesn't Exist`);
+//       else {
+//         await this.setState({
+//           ...this.state,
+//           baseURL: this.state.page.prev
+//         })
+//         this.fetchData()
+//       }
+//       break;
+//     case 'next':
+//       if (!this.state.page.next) console.log(`Page Doesn't Exist`);
+//       else {
+//         await this.setState({
+//           ...this.state,
+//           baseURL: this.state.page.next
+//         })
+//         this.fetchData()
+//       }
+//       break;
+//     case 'last':
+//       if (!this.state.page.last) console.log(`Page Doesn't Exist`);
+//       else {
+//         await this.setState({
+//           ...this.state,
+//           baseURL: this.state.page.last
+//         })
+//         this.fetchData()
+//       }
+//       break;
+//     default:
+//       break;
+//   }
+// }
+
+//   componentDidMount () {
+//     this.fetchData ()
+//   }
+ 
+//   render () {
+//     const { animeList } = this.state;
+//     return (
+//       <div>
+//         <h1 className="page-title text-center">OnioNime</h1>
+//         <Container className="text-center">
+//           <center>
+//             <SearchForm search={this.search}/>
+//           </center>
+//           {/* tombol page ini nanti sepertinya bisa dipecah jadi component */}
+//           <div className="btn-group">
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('first')}>First</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('prev')}>&lt;&lt; Prev</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('next')}>Next &gt;&gt;</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('last')}>Last</Button>
+//           </div>
+//           <div className="d-flex flex-wrap justify-content-center">
+//             {
+//               animeList.map(anime => {
+//                 return (
+//                   <AnimeDetail
+//                     anime={anime}
+//                     key={anime.id}
+//                   />
+//                 )
+//               })
+//             }
+//           </div>
+//           <div className="btn-group">
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('first')}>First</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('prev')}>&lt;&lt; Prev</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('next')}>Next &gt;&gt;</Button>
+//             <Button style={{"fontSize":"10px"}} variant="secondary" onClick={() => this.changePage('last')}>Last</Button>
+//           </div>
+//           </Container>
+//       </div>
+//     )
+//   }
+// }
--- my-react/src/components/AnimeDetail.jsx
+++ my-react/src/components/AnimeDetail.jsx
@@ -1,37 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, Button } from 'react-bootstrap';
 
-class AnimeDetail extends React.Component {
-  formatDate (date) {
+export default function AnimeDetail (props) {
+  const { anime } = props;
+
+  function formatDate (date) {
     const options = {
       year: "numeric",
       month: "short",
       day: "numeric"
     };
-
     let format = new Date(date)
     return format.toLocaleString("en", options)
   }
 
-  render () {
-    const { anime } = this.props
-    return (
-      <>
-        <Card className="m-1" style={{ width: '16rem' }}>
-          <Card.Img className="card-img" variant="top" src={anime.attributes.posterImage.medium} />
-          <Card.Body>
-            <Card.Title className="card-title">{ anime.attributes.canonicalTitle }</Card.Title>
-            <Card.Text className="card-text">
-              Release Date : { this.formatDate(anime.attributes.startDate) } <br/>
-              Status : { anime.attributes.status } <br/>
-              Avg Rating : { !anime.attributes.averageRating ? '-' : anime.attributes.averageRating }
-            </Card.Text>
-            <Button className="btn-favourite" variant="primary">Favourite</Button>
-          </Card.Body>
-        </Card>
-      </>
-    )
-  }
+  return (
+    <>
+      <Card className="m-1" style={{ width: '16rem' }}>
+        <Card.Img className="card-img" variant="top" src={anime.attributes.posterImage.medium} />
+        <Card.Body>
+          <Card.Title className="card-title">{ anime.attributes.canonicalTitle }</Card.Title>
+          <Card.Text className="card-text">
+            Release Date : { formatDate(anime.attributes.startDate) } <br/>
+            Status : { anime.attributes.status } <br/>
+            Avg Rating : { !anime.attributes.averageRating ? '-' : anime.attributes.averageRating }
+          </Card.Text>
+          <Button className="btn-favourite" variant="primary">Favourite</Button>
+        </Card.Body>
+      </Card>
+    </>
+  )
 }
 
-export default AnimeDetail
+// class AnimeDetail extends React.Component {
+//   formatDate (date) {
+//     const options = {
+//       year: "numeric",
+//       month: "short",
+//       day: "numeric"
+//     };
+
+//     let format = new Date(date)
+//     return format.toLocaleString("en", options)
+//   }
+
+//   render () {
+//     const { anime } = this.props
+//     return (
+//       <>
+//         <Card className="m-1" style={{ width: '16rem' }}>
+//           <Card.Img className="card-img" variant="top" src={anime.attributes.posterImage.medium} />
+//           <Card.Body>
+//             <Card.Title className="card-title">{ anime.attributes.canonicalTitle }</Card.Title>
+//             <Card.Text className="card-text">
+//               Release Date : { this.formatDate(anime.attributes.startDate) } <br/>
+//               Status : { anime.attributes.status } <br/>
+//               Avg Rating : { !anime.attributes.averageRating ? '-' : anime.attributes.averageRating }
+//             </Card.Text>
+//             <Button className="btn-favourite" variant="primary">Favourite</Button>
+//           </Card.Body>
+//         </Card>
+//       </>
+//     )
+//   }
+// }
